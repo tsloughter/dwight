7cc020a35ec0a6b2a2f998a009197719616b57b2
Tristan Sloughter <tristan.sloughter@gmail.com>
2012-07-11 17:48:39 -0500

    added handler3

---
+++ lib/dwight_core/src/dwight_core_handler3.erl
@@ -0,0 +1,37 @@
+%%% @author Tristan Sloughter <>
+%%% @copyright (C) 2012, Tristan Sloughter
+%%% @doc
+%%%
+%%% @end
+%%% Created :  9 Jul 2012 by Tristan Sloughter <>
+-module(dwight_core_handler3).
+
+-export([init/3, 
+         handle/2, 
+         terminate/2]).
+
+-record(state, {}).
+
+init({tcp, http}, Req, _Opts) ->
+    {ok, Req, #state{}}.
+
+handle(Req, State) ->
+    dwight_core_utils:make_request(fun handle_request/7, Req, State).
+
+terminate(_Req, _State) ->
+    ok.
+
+%%
+%% Internal functions
+%%
+
+handle_request(Method, Host, Port, Headers, Path, Req, State) ->
+    {ok, Pid} =
+        dwight_core_req_server:setup(atom_to_list(Method), Host, Port, Headers, Path),
+    {RespStatus, RespHeaders, RespBody} = 
+        dwight_core_req_server:send_body(Pid, <<"">>),
+
+    {ok, Req2} = cowboy_http_req:set_resp_body(RespBody, Req),
+    {ok, Req3} = cowboy_http_req:reply(RespStatus, RespHeaders, Req2),
+
+    {ok, Req3, State}.
